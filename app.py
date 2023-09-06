d83532c9ea62ebd4b749ed6ee6f41fad2807f43c
Aluno Lab 135 <lab135@lab.senai>
2023-09-06 16:59:08 -0300

    Segunda Atualizaçao

--- app.py
+++ app.py
@@ -1,3 +1,11 @@
 a=int(input("Digite o Seu primeiro valor: "))
+c=input("Escolha sua operação: ")
 b=int(input("Digite o Seu primeiro valor: "))
-print("Sua Soma e de :",a+b)
+if c == "+" :
+    print("Sua Soma e de :",a+b)
+if c == "-" :
+    print("Sua Soma e de :",a-b)
+if c == "*" :
+    print("Sua Soma e de :",a*b)
+if c == "/" :
+    print("Sua Soma e de :",a/b)
